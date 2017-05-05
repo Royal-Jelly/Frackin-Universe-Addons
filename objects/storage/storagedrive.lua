e00e5111cf3a5d84ef76f17dc0e4e9102435349e
Royal~Jelly <joshuaannandoolittle@gmail.com>
2017-05-05 16:18:23 -0500

    Thanks to Kherae ~ I wouldn't have made it this far without you
    Updates to the storage drive scripts, the input/output nodes can tranfer items.
    Eventually to offset how OP the storage is it will become a requirment to power the drive to acess its contents.

--- objects/storage/storagedrive.lua
+++ objects/storage/storagedrive.lua
@@ -1,3 +1,26 @@
+require "/scripts/kheAA/transferUtil.lua"
+
+ic = {}
+ic.serverVersion = 1
+
+local deltaTime=1
+
+function init()
+	ic.objectName = config.getParameter("shortdescription") or object.name()
+	ic.locked = 0
+	ic.eid = entity.id()
+	ic.dt = 0
+	object.setConfigParameter("icVersion", ic.serverVersion)
+	message.setHandler("lock", ic.handlerLock)
+	message.setHandler("rename", ic.handlerRename)
+	message.setHandler("open", ic.handlerOpen)
+	transferUtil.init()
+	storage.receiveItems=true
+	inDataNode=0
+	outDataNode=0
+end
+
+--don't knwo why we need this snippit because the whole script will only run on SB 1.2+
 function configParameter(name, default)
   if entity.configParameter then
     return entity.configParameter(name,default)--Glad
@@ -14,11 +37,60 @@ function setAnimationState(name, default)
   end
 end
 
-function update() -- cheerful doesnt have container callback :( poll it every half sec then
-  script.setUpdateDelta(30)
-  binInventoryChange()
+function update(dt)
+	if deltaTime > 1 then
+		deltaTime=0
+		transferUtil.loadSelfContainer() -- my Goodness how does one forget something so simple
+		binInventoryChange()  --borrowed from gardenbot
+	else
+		deltaTime=deltaTime+dt
+	end
+
+	-- Persistant Inventory Script Credits to 
+	if icHookUpdate then
+		icHookUpdate(dt)
+	end
+	script.setUpdateDelta(60)
+	if not storage.fixBreak then
+		object.setConfigParameter("smashOnBreak", true)
+		storage.fixBreak = true
+	end
+	if not storage.init then
+		local items = world.getObjectParameter(ic.eid, "content")
+		if items and type(items) == "table" then
+			for k,v in pairs(items) do
+				local leftover = world.containerItemApply(ic.eid, v, k-1)
+				if leftover then
+					world.spawnItem(leftover.name, entity.position(), leftover.count, leftover.parameters)
+				end
+			end
+		end
+		object.setConfigParameter("content", {})
+		storage.init = true
+	end
+	if ic.locked > 0 then
+		ic.locked = ic.locked - dt
+		if ic.locked <= 0 then
+			ic.locked = 0
+			object.setConfigParameter("icLocked", 0)
+		end
+	end
+	ic.dt = ic.dt + dt
+	if ic.dt > 2 then
+		ic.dt = 0
+		local items = world.containerItems(ic.eid)
+		local pos = entity.position()
+		pos[2] = pos[2] + 1
+		for k,v in pairs(items) do
+			if v.parameters.content then
+				world.spawnItem(v.name, pos, v.count, v.parameters)
+				world.containerTakeAt(ic.eid, k-1)
+			end
+		end
+	end
 end
 
+--Animations from Gardenbot mod
 function binInventoryChange()
   if entity.id() then
     local container = entity.id()
@@ -42,4 +114,76 @@ function fillPercent(container)
     end
   end
   return (count/size)
+end
+
+
+-- More persistant storage code 
+function die()
+	local newPrice = 0
+	local countItems = 0
+	local countSlots = 0
+	local spawned = false
+	local opened = world.getObjectParameter(ic.eid, "playerInteract")
+	if opened then
+		local items = world.containerItems(ic.eid)
+		world.containerTakeAll(ic.eid)
+		for slotId,slotItem in pairs(items) do
+			local pc = root.itemConfig(slotItem)
+			if pc and pc.config and pc.config.price then
+				newPrice = newPrice + (pc.config.price * slotItem.count)
+			end
+			countSlots = countSlots + 1
+			countItems = countItems + slotItem.count
+			if slotItem.parameters and slotItem.parameters.content then
+				world.spawnItem(slotItem.name, entity.position(), slotItem.count, slotItem.parameters)
+				items[slotId] = nil
+			end
+		end
+		if countSlots > 0 then
+			local newObj = { shortdescription=ic.objectName, playerInteract=true, content=items }
+			local iconf = root.itemConfig( object.name() )
+			if iconf and iconf.config then
+				if iconf.config.inventoryIcon then
+					newObj.inventoryIcon = iconf.config.inventoryIcon.."?border=1;FF0000?fade=FF0000FF;0.1"
+				end
+				-- if iconf.config.itemAgeMultiplier then
+				-- 	newObj.itemAgeMultiplier = iconf.config.itemAgeMultiplier
+				-- end
+				if newPrice > 0 then
+					if iconf.config.price then
+						newPrice = newPrice + iconf.config.price
+					end
+					newObj.price = newPrice
+				end
+			end
+			newObj.description = "^green;Slots used: ".. countSlots
+			if countItems > countSlots then
+				newObj.description = newObj.description .. "\nTotal items: ".. countItems
+			end
+			world.spawnItem(object.name(), entity.position(), 1, newObj)
+			--object.smash(true)
+			spawned = true
+		end
+	end
+	if spawned == false then
+		world.spawnItem(object.name(), entity.position(), 1)
+	end
+end
+
+function ic.handlerLock()
+	ic.locked = 1
+	object.setConfigParameter("icLocked", ic.locked)
+end
+
+function ic.handlerRename(_, _, newName)
+	if not newName or newName == "" then
+		local default = root.itemConfig(object.name())
+		newName = default.config.shortdescription
+	end
+	ic.objectName = newName
+	object.setConfigParameter("shortdescription", newName)
+end
+
+function ic.handlerOpen()
+	object.setConfigParameter("playerInteract", true)
 end
