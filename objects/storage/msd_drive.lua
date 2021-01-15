16f24f21be85641eda71ccde00d83bb4b76e1165
Royal-Jelly <joshuaannandoolittle@gmailcom>
2021-01-14 21:39:55 -0500

    Experimental branch A matter storage device should have the following features:  - FU Power Network with internal battery buffer/decay rate  - persistant item storage / Power storage  - Fu Item Network to access inventory [as requirement]
    Currently nothing works someone send help

---
+++ objects/storage/msd_drive.lua
@@ -0,0 +1,154 @@
+require '/scripts/fupower.lua'
+require '/scripts/util.lua'
+require "/scripts/kheAA/transferUtil.lua"
+
+
+local batteryUpdateThrottleBase=0.1
+
+function init()
+	power.init()
+	power.setPower(0)
+	power.setMaxEnergy(config.getParameter('isn_batteryCapacity'))
+	if config.getParameter('isnStoredPower') then
+		power.setStoredEnergy(config.getParameter('isnStoredPower'))
+		object.setConfigParameter('isnStoredPower', nil)
+	end
+	transferUtil.init()
+	storage.receiveItems=true
+	inDataNode=0
+	outDataNode=0
+	object.setInteractive(true)
+end
+
+function update(dt)
+	transferUtil.loadSelfContainer() -- my Goodness how does one forget something so simple
+	binInventoryChange()  --borrowed from gardenbot
+	batteryUpdate(dt)
+	power.update(dt)
+end
+
+function die()
+	if power.getStoredEnergy() > 0 then
+		local charge = power.getStoredEnergy() / power.getMaxEnergy() * 100
+		local iConf = root.itemConfig(object.name())
+		local newObject = { isnStoredPower = power.getStoredEnergy() }
+
+		if iConf and iConf.config then
+			-- set the border colour according to the charge level (red → yellow → green)
+			if iConf.config.inventoryIcon then
+				local colour
+
+				if charge <	25 then colour = 'FF0000'
+				elseif charge <	50 then colour = 'FF8000'
+				elseif charge <	75 then colour = 'FFFF00'
+				elseif charge < 100 then colour = '80FF00'
+				else colour = '00FF00'
+				end
+				newObject.inventoryIcon = iConf.config.inventoryIcon .. '?border=1;' .. colour .. '?fade=' .. colour .. 'FF;0.1'
+			end
+			newObject.description = isn_makeBatteryDescription(iConf.config.description or '', charge,true)
+		end
+
+		world.spawnItem(object.name(), entity.position(), 1, newObject)
+	else
+		world.spawnItem(object.name(), entity.position())
+	end
+end
+
+--Animations from Gardenbot mod
+function binInventoryChange()
+  if entity.id() then
+    local container = entity.id()
+    local frames = configParameter("storage", 10)
+    local fill = math.ceil(frames * fillPercent(container))
+    if self.fill ~= fill then
+      self.fill = fill
+      setAnimationState("storage", tostring(fill))
+    end
+  end
+end
+
+function fillPercent(container)
+  if type(container) ~= "number" then return nil end
+  local size = world.containerSize(container)
+  local count = 0
+  for i = 0,size,1 do
+    local item = world.containerItemAt(container, i)
+    if item ~= nil then
+      count = count + 1
+    end
+  end
+  return (count/size)
+end
+
+function isn_makeBatteryDescription(desc, charge, onDeath)
+	if desc == nil then
+		desc = root.itemConfig(object.name())
+		desc = desc and desc.config and desc.config.description or ''
+	end
+	charge = charge or power.getStoredEnergy() / power.getMaxEnergy() * 100
+
+	-- bat flattery
+	if charge == 0 then return desc end
+
+	-- round down to multiple of 0.5 (special case if < 0.5)
+	if charge < 0.5 then
+		charge = '< 0.5'
+	else
+		charge = math.floor (charge * 2) / 2
+	end
+
+	-- append charge state to default description; ensure that it's on a line of its own
+	local str=string.split(desc,"^truncate;")
+	if str[1] then str=str[1] else str="" end
+	str=str..((onDeath and " ^red;Interact for Info^reset;") or "\n^blue;Input 1^reset;: Power Switch\n^blue;Input^white;1^blue;: ^white;Item Network\n^red;Output^white;1^red;: ^white;item network")
+	str = str .. (desc ~= '' and "\n" or '') .. "Power Stored: ^yellow;"..util.round(power.getStoredEnergy(),1).."^reset;/^green;"..util.round(power.getMaxEnergy(),1).."^reset;J (^yellow;" .. charge .. '^reset;%)'
+	return str
+end
+
+function string.split(str, pat)
+	 local t = {}	-- NOTE: use {n = 0} in Lua-5.0
+	 local fpat = "(.-)" .. pat
+	 local last_end = 1
+	 local s, e, cap = str:find(fpat, 1)
+	 while s do
+		if s ~= 1 or cap ~= "" then
+			 table.insert(t, cap)
+		end
+		last_end = e+1
+		s, e, cap = str:find(fpat, last_end)
+	 end
+	 if last_end <= #str then
+			cap = str:sub(last_end)
+			table.insert(t, cap)
+	 end
+	 return t
+end
+
+local oldPowerRemove=power.remove
+function power.remove(...)
+	--local on=not object.isInputNodeConnected(0) or object.getInputNodeLevel(0)
+	oldPowerRemove(...)
+	batteryUpdate()
+end
+
+function batteryUpdate(dt)
+	batteryUpdateThrottle=math.max(0,(batteryUpdateThrottle or batteryUpdateThrottleBase)-(dt or 0))
+	local on=(not object.isInputNodeConnected(0)) or object.getInputNodeLevel(0)
+	power.setPower(on and power.getStoredEnergy() or 0)
+	if batteryUpdateThrottle <= 0 then
+		object.setConfigParameter('description', isn_makeBatteryDescription())
+		local oldAnim
+		if self.oldPowerStored then
+			oldAnim=((self.oldPowerStored) == 0 and 'd' or tostring(math.floor(math.min((self.oldPowerStored) / power.getMaxEnergy(),1.0) * 10),10))
+		end
+		local newAnim=(power.getStoredEnergy() == 0 and 'd' or tostring(math.floor(math.min(power.getStoredEnergy() / power.getMaxEnergy(),1.0) * 10),10))
+		if oldAnim~=newAnim then
+			animator.setAnimationState("meter",newAnim)
+		end
+		self.oldPowerStored=power.getStoredEnergy()
+		batteryUpdateThrottle=batteryUpdateThrottleBase
+		object.setOutputNodeLevel(0,self.oldPowerStored>0)
+		object.setOutputNodeLevel(1,power.getStoredEnergy() >= power.getMaxEnergy())
+	end
+end
